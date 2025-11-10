66a0f4ad9e0c5920ec3702635d784d5b90194851
KanonMinematsu <fko2347044@stu.o-hara.ac.jp>
2025-11-10 15:03:35 +0900

    storeのbase

--- app/urls.py
+++ app/urls.py
@@ -2,10 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.userhome, name='home'),
-    path('category/', views.category, name='category'),
-    path('cart/', views.cart, name='cart'),
-    path('history/', views.history, name='history'),
-    path('mypage/', views.usermypage, name='mypage'),
-    path('alert/', views.useralert, name='alert'),
+    path('', views.top, name='top'),
+
+    path('user_home/', views.user_home, name='user_home'),
+    path('category/', views.user_category, name='user_category'),
+    path('cart/', views.user_cart, name='user_cart'),
+    path('history/', views.user_history, name='user_history'),
+    path('user_mypage/', views.user_mypage, name='user_mypage'),
+    path('user_alert/', views.user_alert, name='user_alert'),
+
+    path('store_home/', views.store_home, name='store_home'),
+    path('list/', views.store_list, name='store_list'),
+    path('store_mypage/', views.store_mypage, name='store_mypage'),
+    path('store_alert/', views.store_alert, name='store_alert'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,21 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def userhome(request):
+def top(request):
+    return render(request, 'top.html')
+
+# user側のビュー
+def user_home(request):
     return render(request, 'user/home.html')
 
-def category(request):
+def user_category(request):
     return render(request, 'user/category.html')
 
-def cart(request):
+def user_cart(request):
     return render(request, 'user/cart.html')
 
-def history(request):
+def user_history(request):
     return render(request, 'user/history.html')
 
 
-def usermypage(request):
+def user_mypage(request):
     return render(request, 'user/mypage.html')
 
-def useralert(request):
+def user_alert(request):
     return render(request, 'user/alert.html')
+
+# store側のビュー
+def store_home(request):
+    return render(request, 'store/home.html')
+
+def store_list(request):
+    return render(request, 'store/list.html')
+
+def store_mypage(request):
+    return render(request, 'store/mypage.html')
+
+def store_alert(request):
+    return render(request, 'store/alert.html')
