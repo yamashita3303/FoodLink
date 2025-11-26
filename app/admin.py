d9cdb1f825f9350202a01e96bb497883147aab9d
yamashita <fko2347040@stu.o-hara.ac.jp>
2025-11-26 10:39:06 +0900

    orderのadmin追加

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import User, Product, Category
+from .models import User, Product, Category, Order, OrderItem
 
 admin.site.register(User)
 admin.site.register(Product)
 admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(OrderItem)
